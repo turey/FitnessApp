2dca8c925d09356268c05c5a60f528522a8dbb88
Bishal Wagle <bishalw@gmail.com>
2017-11-23 11:56:17 -0800

    Initial Commit

---
+++ FitnessApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FitnessApp
+//
+//  Created by Bishal Wagle on 11/23/17.
+//  Copyright © 2017 FitnessGroup. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
